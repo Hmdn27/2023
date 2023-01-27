69de41f3fc7d17061c4f190f861f62e315733f41
Hmdn27 <110008232+Hmdn27@users.noreply.github.com>
2023-01-27 09:17:09 +0700

    Add files via upload
    Composer/ Jumat 27 Januari 2023

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+require 'vendor/autoload.php';
+
+// reference the Dompdf namespace
+use Dompdf\Dompdf;
+
+$faker = Faker\Factory::create();
+$dompdf = new Dompdf();
+
+$Nama = $faker->name();
+$Alamat = $faker->text();
+
+$dompdf->loadHtml("
+    <table border='1'>
+        <tr>
+           <td>Nama</td>
+           <td>Alamat</td>
+        </tr>
+        <tr>
+           <td>".$Nama."</td>
+           <td>".$Alamat."</td>
+        </tr>
+    </table>");
+
+// (Optional) Setup the paper size and orientation
+$dompdf->setPaper('A4', 'landscape');
+
+// Render the HTML as PDF
+$dompdf->render();
+
+// Output the generated PDF to Browser
+$dompdf->stream();
